8ff3a0fc6a5a1b8e333173a20587e23d42bbf621
emsevgiunal <c0055427@newcastle.ac.uk>
2025-10-01 13:22:31 +0100

    fix routing

--- newspringlondon/src/index.js
+++ newspringlondon/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Derive a pathname from PUBLIC_URL if present, else use "/"
+const basename = process.env.PUBLIC_URL
+  ? new URL(process.env.PUBLIC_URL).pathname || "/"
+  : "/";
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={basename}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
