7481807610b52ce70e9cfed7138abc3545ba3e63
emsevgiunal <c0055427@newcastle.ac.uk>
2025-10-01 14:34:43 +0100

    fix bugs

--- newspringlondon/src/App.js
+++ newspringlondon/src/App.js
@@ -25,8 +25,6 @@ function App() {
       <Footer />
     </Router>
 
-      
-
     </>
   );
 }
--- newspringlondon/src/index.js
+++ newspringlondon/src/index.js
@@ -6,7 +6,5 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename="/">
     <App />
-  </BrowserRouter>
 );
